e13cb6b4803f83728632ddb3e1ff9b2b22560d99
Andhika Prasetya <andhikapr081@gmail.com>
2023-10-03 19:54:54 +0700

    refactor: disabled level options that is still locked

--- source/src/components/LevelOptions/index.tsx
+++ source/src/components/LevelOptions/index.tsx
@@ -7,19 +7,20 @@ type props = {
 
 const LevelOptions: React.FC<props> = ({ active, text }) => {
   return (
-    <div
+    <button
+      disabled={!active}
       className={`${
         active ? "bg-[#C0EEF2]" : "bg-[#D9D9D9] border-[3px] border-[#9D9D9D]"
       } px-[36px] py-[18px] w-full rounded-[20px]`}
     >
       <p
-        className={`font-poppins text-[20px] font-bold ${
+        className={`font-poppins text-[20px] text-left font-bold ${
           active ? "text-black" : "text-[#9D9D9D]"
         }`}
       >
         {text}
       </p>
-    </div>
+    </button>
   );
 };
 
