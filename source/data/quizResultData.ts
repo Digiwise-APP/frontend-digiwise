a57dbe029543b2b493f777d3613060eca9ab51bb
Andhika Prasetya <andhikapr081@gmail.com>
2023-10-06 20:36:43 +0700

    feat: add static data for quiz result

---
+++ source/data/quizResultData.ts
@@ -0,0 +1,20 @@
+const results = [
+    {
+        first_level: {
+            title: '#1 - BBB: Bukan Baca Biasa',
+            success: {
+                salutation: 'Kemampuan yang luar biasa, Zie!',
+                first_paragraph: "Kamu telah berhasil menyelesaikan level 'BBB: Bukan Baca Biasa' dengan skor 80. Kerja bagus! 🎉 Kamu telah menunjukkan kemampuanmu dalam menempatkan diri di antara berita palsu dan informasi yang benar, bahkan saat berada kondisi masih membingungkan. Saat kamu melanjutkan perjalanan ini, ingatlah untuk menerapkan pengetahuan baru yang kamu dapatkan untuk tetap terinformasi dan melindungi dirimu dari informasi yang salah. Tetaplah semangat untuk menyelesaikannya dengan baik!",
+                second_paragraph: 'Jika kamu siap, kamu bisa lanjut ke level berikutnya dan terus mengasah kebijaksanaan digitalmu. Kamu behak mendapatkan pujian!'
+            },
+            failed: {
+                salutation: 'Usaha yang bagus, Zie!',
+                first_paragraph: "Tampaknya kamu belum mencapai skor minimum 70 di level 'BBB: Bukan Baca Biasa'. Tidak masalah! 🌟 Ini hanya awal dari perjalananmu, dan pembelajaran memerlukan waktu. Jangan berkecil hati; sebaliknya, gunakan ini sebagai kesempatan untuk memperkuat keterampilanmu. Kamu selalu bisa kembali ketika kamu siap untuk mencoba lagi. Kami percaya pada potensimu untuk menjadi pembaca yang kritis dan cerdas di era digital ini. Luangkan waktu untuk beristirahat sejenak, dan ketika kamu merasa siap, klik 'Ulangi Kuis' untuk mencoba lagi. Kemajuanmu penting bagi kami, jadi teruslah maju, dan kamu akan berhasil membuktikan bahwa kamu salah satu dari si bijak digital. Semangat, dan sampai jumpa di level berikutnya!",
+            }
+            
+          
+        }
+    }
+]
+
+export default results
