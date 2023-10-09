ca53b96f9699be7e95fc1a5b8248a82fd1bc383c
Andhika Prasetya <andhikapr081@gmail.com>
2023-10-09 19:17:57 +0700

    feat: add static content for level preparation and result

--- source/data/quizText.ts
+++ source/data/quizText.ts
@@ -18,6 +18,23 @@ const text = [
             }
             
           
+        },
+        second_level: {
+            title: '#2 - Mengungkap Manipulasi Konten',
+            preparation: {
+                salutation: "Selamat datang di level 'Mengungkap Manipulasi Konten'",
+                first_paragraph: "Siap untuk menjelajahi dunia konten digital yang mengelabui? Mari uji kemampuanmu dalam mengidentifikasi hoaks dan disinformasi visual. Di level ini, kita akan menggali lebih dalam tentang bagaimana gambar dan video dapat dimanipulasi untuk menyesatkan.Misimu adalah mencapai skor minimal 70 untuk membuka level berikutnya. Kamu tetap bebas keluar dari kuis kapan saja, tetapi ingatlah bahwa progres pengerjaan tidak akan disimpan.",
+                second_paragraph: "Pastikan kamu tetap tajam dan kritis dalam melihat setiap konten yang kamu temui. Selamat belajar!"
+            },
+            success: {
+                salutation: 'Prestasi yang luar biasa',
+                first_paragraph: "Kamu telah berhasil menyelesaikan Level 2 ini dengan skor 80. Hebat! 🎉 Kemampuanmu dalam mengidentifikasi manipulasi konten visual sangat mengesankan. Kamu sudah memahami betapa pentingnya waspada terhadap gambar dan video yang dimanipulasi dengan maksud menyesatkan. Dalam perjalananmu selanjutnya, pastikan untuk terus mengasah kemampuanmu dan membagikan pengetahuan ini kepada orang lain. Tetap jadi pembaca yang tajam dan kritis di era digital ini!. Jika kamu siap, lanjutkan ke Level berikutnya dan terus tingkatkan kebijaksanaan digitalmu. Kamu sudah membuktikan bahwa kamu layak mendapatkan apresiasi ini!"
+            },
+            failed: {
+                salutation: 'Usaha yang baik',
+                first_paragraph: "Sayangnya, tampaknya kamu belum mencapai skor minimum 70 di Level 2 ini. Tidak perlu khawatir! 🌟 Belajar adalah proses, dan kamu masih memiliki kesempatan untuk memperbaiki kemampuanmu. Jangan kecewa; sebaliknya, gunakan ini sebagai motivasi untuk lebih memahami cara mengidentifikasi manipulasi konten visual.",
+                second_paragraph: "Kamu selalu bisa kembali dan mencoba lagi ketika kamu merasa siap. Kami yakin kamu akan berhasil menjadi ahli dalam mengungkap manipulasi konten dan melindungi dirimu dari hoaks visual di masa depan. Tetap semangat, dan sampai jumpa di level berikutnya!. Semoga Good Ending dan Sad Ending ini sesuai dengan apa yang Anda inginkan untuk Level 2 kuis Anda!"
+            }
         }
     }
 ]
