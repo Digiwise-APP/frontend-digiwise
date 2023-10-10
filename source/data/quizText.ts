ec8b600d46645ad8aeb846af5c91670a93498dd0
Andhika Prasetya <andhikapr081@gmail.com>
2023-10-10 16:16:13 +0700

    feat: add more static content for quiz preparation and result

--- source/data/quizText.ts
+++ source/data/quizText.ts
@@ -35,6 +35,41 @@ const text = [
                 first_paragraph: "Sayangnya, tampaknya kamu belum mencapai skor minimum 70 di Level 2 ini. Tidak perlu khawatir! 🌟 Belajar adalah proses, dan kamu masih memiliki kesempatan untuk memperbaiki kemampuanmu. Jangan kecewa; sebaliknya, gunakan ini sebagai motivasi untuk lebih memahami cara mengidentifikasi manipulasi konten visual.",
                 second_paragraph: "Kamu selalu bisa kembali dan mencoba lagi ketika kamu merasa siap. Kami yakin kamu akan berhasil menjadi ahli dalam mengungkap manipulasi konten dan melindungi dirimu dari hoaks visual di masa depan. Tetap semangat, dan sampai jumpa di level berikutnya!. Semoga Good Ending dan Sad Ending ini sesuai dengan apa yang Anda inginkan untuk Level 2 kuis Anda!"
             }
+        },
+        third_level: {
+            title: '#3 - Duet Maut: Phishing dan Scam',
+            preparation: {
+                salutation: "Selamat datang di level 'Duet Maut: Phishing dan Scam'",
+                first_paragraph: "Apakah kamu siap untuk menjelajahi dunia phishing dan penipuan daring? 🎣 Kamu sudah sampai pada level yang lebih menantang dalam perjalananmu memahami bahaya phishing dan scam. Di sini, kamu akan lebih mendalami cara mengidentifikasi dan melindungi diri dari upaya penipuan online yang semakin canggih. Misimu tetap sama, yaitu capai skor minimal 70 untuk membuka pintu menuju level berikutnya. Namun, pada level ini berhati-hatilah pada pilihan yang salah dan merugikanmu. Kamu bebas untuk keluar dari kuis kapan saja, namun perlu diingat, progresmu tidak akan disimpan.",
+                second_paragraph: "Kamu sudah berada di jalur yang benar untuk menjadi bijak dan cerdas dalam menjelajahi dunia digital. Selamat bersiap untuk tantangan yang lebih besar dan selamat belajar!"
+            },
+            success: {
+                salutation: 'Prestasi yang luar biasa',
+                first_paragraph: "Kamu telah menyelesaikan level ini dengan prestasi gemilang, meraih skor 85! 🌟 Ini menunjukkan bahwa kamu benar-benar paham mengenai bahaya phishing dan scam di dunia digital. Sebagai seorang bijak digital, kamu telah memahami cara melindungi dirimu sendiri dan orang lain dari upaya penipuan yang semakin canggih. Kamu juga telah membuktikan bahwa kecerdasanmu lebih unggul dibandingkan dengan trik-trik penipu digital. Saat kamu melanjutkan perjalanan ini, jangan ragu untuk terus mengasah kemampuanmu. Dunia digital terus berkembang, dan kamu sudah siap menghadapinya. Semoga kebijaksanaanmu tetap bersinar terang di era digital yang penuh tantangan ini!"
+            },
+            failed: {
+                salutation: 'Usahamu patut diacungi jempol',
+                first_paragraph: "Meskipun kamu belum mencapai skor minimum 70 di level ini, ingat ini bukanlah akhir dari perjalananmu. 🌟 Pembelajaran memerlukan waktu, dan kamu telah mendapatkan pengalaman berharga. Jangan berkecil hati; sebaliknya, gunakan ini sebagai kesempatan untuk lebih memahami bahaya phishing dan scam. Kamu selalu bisa kembali ketika merasa siap untuk mencoba lagi. Teruslah belajar dan teruslah berusaha, karena kamu punya potensi untuk menjadi ahli dalam menjaga diri dari penipuan online yang semakin canggih.",
+                second_paragraph: "Istirahat sejenak, dan saat kamu merasa siap, klik 'Ulangi Kuis' untuk melanjutkan perjalananmu. Kami percaya kamu bisa melakukannya! Semangat, dan sampai jumpa di level berikutnya!"
+            }
+        },
+        fourth_level: {
+            title: '#4 - Membongkar Taktik Perusak Reputasi',
+            preparation: {
+                salutation: "Selamat datang di level 'Membongkar Taktik Perusak Reputasi'",
+                first_paragraph: "Kini saatnya untuk benar-benar mendalam dan mengidentifikasi taktik-taktik yang digunakan untuk merusak reputasi dalam berita dan informasi 📑. Di sini, kamu akan dihadapkan pada berbagai situasi yang menuntutmu untuk berpikir kritis dan mengungkap cara-cara manipulatif yang digunakan oleh informasi yang tidak dapat dipercaya. Misimu tetap sama, yaitu mencapai skor minimal 70 untuk membuka pintu menuju level berikutnya. Namun, peringatan penting: pilihan yang salah dapat mengurangi skormu. Jadi, pilihlah dengan hati-hati!",
+                second_paragraph: "Kamu siap untuk mengungkap hasil dari taktik ini dan melindungi diri dari informasi yang merugikan. Selamat menempuh perjalanan ini, dan tetap berpikir kritis!"
+            },
+            success: {
+                salutation: 'Pencapaian yang luar biasa',
+                first_paragraph: "Kamu telah berhasil menyelesaikan level ini dengan skor 85. Kerja bagus! 🎉 Kamu telah menguasai keterampilan kritis dalam mengenali taktik-taktik yang digunakan untuk merusak reputasi dalam berita dan informasi. Ini akan menjadi alat yang sangat berharga dalam menjelajahi dunia digital yang penuh dengan informasi yang menyesatkan. Selamat, dan teruslah mengasah keterampilanmu!",
+                second_paragraph: "Jika kamu siap, lanjutkan perjalananmu dengan kebijaksanaan digital di level berikutnya. Selamat tinggal dan sampai jumpa!"
+            },
+            failed: {
+                salutation: 'Usaha yang bagus',
+                first_paragraph: "Namun, tampaknya kamu belum mencapai skor minimum 70 di level 'Membongkar Taktik Merusak Reputasi'. Tidak perlu khawatir! 🌟 Gunakan ini sebagai kesempatan untuk memperkuat keterampilanmu. Kamu selalu bisa kembali ketika kamu merasa siap untuk mencoba lagi. Kami yakin bahwa kamu memiliki potensi untuk menguasai taktik-taktik ini dan melindungi diri dari informasi yang merugikan. Luangkan waktu untuk beristirahat sejenak, dan ketika kamu siap, klik 'Ulangi Kuis' untuk mencoba lagi.",
+                second_paragraph: "Kami sangat menghargai kemajuanmu, jadi teruslah maju, dan kamu akan berhasil membuktikan bahwa kamu salah satu dari yang bijak digital. Semangat, dan sampai jumpa di level berikutnya!"
+            }
         }
     }
 ]
