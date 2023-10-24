7e5d91649f28cecb9b951198ed2b87056d2a0bc9
Andhika-GIT <andhikapr081@gmail.com>
2023-10-24 22:36:37 +0700

    feat: add new function to send user answer and get question

--- source/src/api/quiz.ts
+++ source/src/api/quiz.ts
@@ -1,23 +1,62 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 import { BASE_URL } from "./config";
 
-type Question = {
+type QuestionProps = {
   id: number;
   type: string;
   level: number;
-  question: string;
-  option: string[];
-  image?: string;
+  question_type: string;
+  option: string;
+  url_image?: string;
 };
 
-export const getQuizByLevel = async (level: number): Promise<Question[]> => {
-  const { data } = await axios.get<Question[]>(
-    `${BASE_URL}/api/v1/users/questions`,
+type UserAnswerProps = {
+  level: number;
+  type: string;
+  answers: Array<{
+    question_id: number;
+    answer: string | string[];
+  }>;
+};
+
+type QuizResultResponse = {
+  score: number;
+};
+
+const TEMP_TOKEN =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjUyOTA1M2IzMjIzODJiM2VhMDEwNWYxIiwiZW1haWwiOiJjb250b2hAZ21haWwuY29tIiwidXNlcm5hbWUiOiJuaXNhIiwiaWF0IjoxNjk4MTU4NjI3LCJleHAiOjE2OTgxNjIyMjd9.cB6o-Fg0nWm_zVPgvRr5EmWa60kR6Lol6zqafrgDMbU";
+
+export const getQuizByLevel = async (
+  level: number,
+): Promise<QuestionProps[]> => {
+  const { data } = await axios.get<QuestionProps[]>(
+    `${BASE_URL}users/questions`,
     {
-      headers: {
+      params: {
         level: level,
+      },
+      headers: {
+        Authorization: TEMP_TOKEN,
       },
     },
   );
+
   return data;
+};
+
+export const sentUserAnswer = async (
+  userAnswer: UserAnswerProps,
+): Promise<number> => {
+  const { data } = await axios.post<
+    UserAnswerProps,
+    AxiosResponse<QuizResultResponse>
+  >(`${BASE_URL}/users/answers`, userAnswer, {
+    headers: {
+      Authorization: TEMP_TOKEN,
+    },
+  });
+
+  const { score } = data;
+
+  return score;
 };
