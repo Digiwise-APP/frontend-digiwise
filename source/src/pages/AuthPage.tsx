8b526e5c15f7dccd368adc10b4287a0c3eb717ab
Andhika Prasetya <andhikapr081@gmail.com>
2023-10-26 20:14:00 +0700

    refactor: update some font and display position

--- source/src/pages/AuthPage.tsx
+++ source/src/pages/AuthPage.tsx
@@ -1,27 +1,31 @@
 import React from "react";
 
+import authImage from "../assets/auth_image_2.jpg";
+
 const AuthPage = () => {
   return (
     <div
       className="relative bg-cover bg-center bg-no-repeat"
       style={{
-        backgroundImage: `url(https://images.unsplash.com/photo-1579621970563-ebec7560ff3e?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1951&amp;q=80)`,
+        backgroundImage: `url(${authImage})`,
       }}
     >
-      <div className="absolute inset-0 z-0 bg-gradient-to-b from-green-500 to-green-400 opacity-75"></div>
-      <div className="mx-0 min-h-screen justify-center sm:flex sm:flex-row">
-        <div className="z-10 flex  flex-col self-center p-10 sm:max-w-5xl  xl:max-w-2xl">
+      <div className="absolute inset-0 z-0 bg-[#232b2b] opacity-[80%]"></div>
+      <div className="mx-0 flex min-h-screen items-center justify-center p-5 sm:flex-row">
+        <div className="z-10 hidden flex-col self-center p-10 sm:flex  md:max-w-5xl xl:max-w-2xl">
           <div className="hidden flex-col self-start text-white  lg:flex">
             <img src="" className="mb-3" />
-            <h1 className="mb-3 text-5xl font-bold">Hi ? Welcome Back Aji </h1>
-            <p className="pr-3">
+            <h1 className="mb-3 text-xl font-bold md:text-5xl">
+              Hi Welcome back
+            </h1>
+            <p className="pr-3 md:text-2xl">
               Lorem ipsum is placeholder text commonly used in the graphic,
               print, and publishing industries for previewing layouts and visual
               mockups
             </p>
           </div>
         </div>
-        <div className="z-10 flex justify-center  self-center">
+        <div className="z-10 flex justify-center self-center">
           <div className="w-100 mx-auto rounded-2xl bg-white p-12 ">
             <div className="mb-4">
               <h3 className="text-2xl font-semibold text-gray-800">Sign In </h3>
@@ -51,7 +55,7 @@ const AuthPage = () => {
               <div>
                 <button
                   type="submit"
-                  className="mt-9 flex w-full cursor-pointer justify-center  rounded-full bg-green-400 p-3  font-semibold tracking-wide text-gray-100  shadow-lg transition duration-500 ease-in hover:bg-green-500"
+                  className="mt-9 flex w-full cursor-pointer justify-center  rounded-full bg-[#2CC6F6] p-3  font-semibold tracking-wide text-gray-100  shadow-lg transition duration-500 ease-in hover:bg-green-500"
                 >
                   Sign in
                 </button>
